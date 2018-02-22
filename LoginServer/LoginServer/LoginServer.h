efd24b872a2d55003b3baaf04a045f0a69e6f29a
진현수 <jhs9253@naver.com>
2018-02-23 04:33:56 +0900

    틀 추가

---
+++ LoginServer/LoginServer/LoginServer.h
@@ -0,0 +1,102 @@
+#pragma once
+
+class CLoginServer : public CNetServer
+{
+public:
+
+	CLoginServer(int iMaxClient);
+	virtual ~CLoginServer();
+
+protected:
+
+	가상함수들...
+
+		//-----------------------------------------------------------
+		// 로그인서버 스케쥴러 역할
+		//-----------------------------------------------------------
+		static unsigned __stdcall	UpdateThread(void *pParam);
+
+	void				Schedule_PlayerTimeout(void);
+	void				Schedule_ServerTimeout(void);
+
+public:
+
+
+	/////////////////////////////////////////////////////////////
+	// 사용자 관리함수들.  
+	/////////////////////////////////////////////////////////////
+
+	/////////////////////////////////////////////////////////////
+	// OnClientJoin, OnClientLeave 에서 호출됨.
+	/////////////////////////////////////////////////////////////
+	bool				InsertPlayer(CLIENT_ID ClientID);
+	bool				RemovePlayer(CLIENT_ID ClientID);
+
+	/////////////////////////////////////////////////////////////
+	// OnRecv 에서 로그인인증 처리 후 사용,  UpdateThread 에서 일정시간 지난 유저에 사용.
+	/////////////////////////////////////////////////////////////
+	bool				DisconnectPlayer(CLIENT_ID ClientID);
+
+	/////////////////////////////////////////////////////////////
+	// 접속 사용자 수 
+	/////////////////////////////////////////////////////////////
+	int				GetPlayerCount(void);
+
+
+	/////////////////////////////////////////////////////////////
+	// White IP 관련
+	/////////////////////////////////////////////////////////////
+
+	..WhiteIP 목록을 저장하구 검색, 확인 할 수 있는 기능
+
+
+
+protected:
+
+	// 로그인 요청 패킷처리
+	bool				PacketProc_ReqLogin(CLIENT_ID ClientID, CPacket *pPacket);
+
+	// 패킷 생성부
+	bool				MakePacket_ResLogin(CPacket *pPacket, __int64 iAccountNo, WCHAR *szID, WCHAR *szNickname, BYTE byStatus);
+
+
+
+protected:
+
+	//-------------------------------------------------------------
+	// 접속자 관리.
+	// 
+	// 접속자는 본 리스트로 관리함.  스레드 동기화를 위해 SRWLock 을 사용한다.
+	//-------------------------------------------------------------
+	list<CPlayer *>			_PlayerList;
+	SRWLOCK				_PlayerList_srwlock;
+
+
+	//-------------------------------------------------------------
+	// 기타 모니터링용 변수,스레드 함수.
+	//-------------------------------------------------------------
+	long				_Monitor_LoginSuccessTPS;
+
+	long				_Monitor_LoginWait;					// 로그인 패킷 수신 후 대기중인 수
+
+	long				_Monitor_LoginProcessTime_Max;		// 로그인 처리 시간 최대
+	long				_Monitor_LoginProcessTime_Min;		// 로그인 처리 시간 최소
+	long long			_Monitor_LoginProcessTime_Total;	// 총 합
+	long long			_Monitor_LoginProcessCall_Total;	// 로그인 처리 요청 총 합
+
+private:
+
+	long				_Monitor_LoginSuccessCounter;
+
+
+	HANDLE				_MonitorTPS_Thread;
+	static unsigned __stdcall	MonitorTPS_Thread(void *pParam);
+	bool				MonitorTPS_Thread_update(void);
+
+};
+
+
+
+
+
+
